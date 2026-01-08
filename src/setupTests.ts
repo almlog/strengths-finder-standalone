fc63f44e724d20ff15e33cbf5cab8d1b4679cb1e
suzuki.shunpei <suzuki.shunpei@altx.co.jp>
2026-01-08 13:03:36 +0900

    Revert "Merge branch 'feature/firebase-authentication' - Firebase Authentication機能を実装"
    This reverts commit 620527fc89e67b709aaff5ad44e0e8cbadc87e7b, reversing
    changes made to 17957e1d4ce4fcaef2e7fc134efa0b4423de46d2.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,9 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// TextEncoder/TextDecoder polyfill for Jest environment
-// Required for react-router v7
-import { TextEncoder, TextDecoder } from 'util';
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder as typeof global.TextDecoder;
